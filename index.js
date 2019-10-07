a41b0324e3dc6a019f3ef2908db95e1060d0158f
elijahmm17 <elijahmm17@gmail.com>
2019-10-07 13:39:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ function map(arr, fn) {
   return result;
 }
 
-function reduce(arr, fn, init) {
+function reduce(arr, fn, init=0) {
   let result = init;
-  let i = 0;
-  if (!(!!init)) {
-    result = arr[0];
-    i = 1;
-  }
-  for (; i < arr.length; i++) {
-    result = fn(result, arr[i]);
+  for (let i = 0; i < arr.length; i++) {
+    result = fn(arr[i], result);
   }
   return result;
 }
